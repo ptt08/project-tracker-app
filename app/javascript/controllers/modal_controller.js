432843d8f06914b05dd9b6042177c6718938be93
Lucca Teruya <supersecureemailaccount@protonmail.com>
2023-06-29 22:08:00 +0900

    Created modal stimulus controller; modal closes with button and when clicked outside the modal

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  static targets = [ 'overlay', 'window' ];
+
+  hideModal() {
+    this.element.parentElement.removeAttribute('src')
+    this.element.remove()
+  }
+
+  checkClick(e) {
+    if (this.overlayTarget.contains(e.target) && !this.windowTarget.contains(e.target)) {
+      this.hideModal();
+    }
+  }
+}
