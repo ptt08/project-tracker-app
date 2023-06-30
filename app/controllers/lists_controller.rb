185ddf907e18dab7a80625b9a4c894906653e612
Lucca Teruya <supersecureemailaccount@protonmail.com>
2023-07-01 00:38:42 +0900

    Created new and create methods in lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,20 @@
 class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
