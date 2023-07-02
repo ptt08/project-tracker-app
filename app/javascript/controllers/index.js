10bc79d9507f9e5995b91437ba6ff52dac6231d2
Lucca Teruya <supersecureemailaccount@protonmail.com>
2023-07-02 19:21:23 +0900

    SortableJS installed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import ModalController from "./modal_controller"
 application.register("modal", ModalController)
+
+import SortableController from "./sortable_controller"
+application.register("sortable", SortableController)
